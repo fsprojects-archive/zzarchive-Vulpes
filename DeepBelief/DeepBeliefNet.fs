72855dbc7fa34a97d919b88000010c118fd1bade
Rob Lyndon <rob.lyndon@gmail.com>
2013-10-21 01:40:54 +0100

    Defined the deep belief training function.

--- DeepBelief/DeepBeliefNet.fs
+++ DeepBelief/DeepBeliefNet.fs
@@ -5,5 +5,12 @@ module DeepBeliefNet =
     open System
     open NeuralNet
 
-    //let dbnSetUp sizes numepochs =
+    let dbnTrain trainLayer stepUp (dbn : List<'a>) xInputs =
+        let start = trainLayer (dbn.Head, xInputs)
+        dbn.Tail |> List.fold(fun (acc : List<'b * 'c>) element -> 
+            let currentTuple = acc.Head
+            let x = stepUp (fst currentTuple) (snd currentTuple)
+            let nextDbn = trainLayer (element, x)
+            (nextDbn, x) :: acc) [(start, xInputs)]
+            |> List.rev
 
