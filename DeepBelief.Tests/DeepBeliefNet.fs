3f53661880ff3955e93fd43cb3824056b5de4cd1
Rob Lyndon <rob.lyndon@gmail.com>
2013-10-25 00:17:45 +0100

    Executed the initialisation of the DBN and tested it.

---
+++ DeepBelief.Tests/DeepBeliefNet.fs
@@ -0,0 +1,59 @@
+﻿namespace DeepBelief.Tests
+
+open Xunit
+open FsUnit.Xunit
+open DeepBelief.DeepBeliefNet
+open MathNet.Numerics.LinearAlgebra.Double
+open MathNet.Numerics.LinearAlgebra.Generic
+open System
+
+type ``Given a Deep belief network with two layers`` ()=
+    let sizes = [100; 100]
+    let alpha = 1.0
+    let momentum = 0.0
+    let xInputs = DenseMatrix.zeroCreate 60000 784
+    let twoLayerDbn = dbn sizes alpha momentum xInputs
+
+    let rows0 = twoLayerDbn.Head.Weights.RowCount
+    let columns0 = twoLayerDbn.Head.Weights.ColumnCount
+    let v0 = twoLayerDbn.Head.VisibleBiases
+    let h0 = twoLayerDbn.Head.HiddenBiases
+
+    let rows1 = twoLayerDbn.[1].Weights.RowCount
+    let columns1 = twoLayerDbn.[1].Weights.ColumnCount
+    let v1 = twoLayerDbn.[1].VisibleBiases
+    let h1 = twoLayerDbn.[1].HiddenBiases
+
+    [<Fact>] member test.
+        ``The length of the DBN should be 2.``()=
+        twoLayerDbn.Length |> should equal 2
+
+    [<Fact>] member test.
+        ``The weights of the first RBM should be a 100 by 784 matrix.``()=
+        (rows0, columns0) |> should equal (100, 784)
+
+    [<Fact>] member test.
+        ``The visible biases of the first RBM should be a vector of length 784.``()=
+        v0.Count |> should equal 784
+
+    [<Fact>] member test.
+        ``The hidden biases of the first RBM should be a vector of length 100.``()=
+        h0.Count |> should equal 784
+
+    [<Fact>] member test.
+        ``The weights of the second RBM should be a 100 by 100 matrix.``()=
+        (rows1, columns1) |> should equal (100, 100)
+
+    [<Fact>] member test.
+        ``The visible biases of the second RBM should be a vector of length 100.``()=
+        v1.Count |> should equal 100
+
+    [<Fact>] member test.
+        ``The hidden biases of the second RBM should be a vector of length 100.``()=
+        h1.Count |> should equal 100
+
+    [<Fact>] member test.
+        ``Each RBM should have zero momentum and unit alpha.``()=
+        twoLayerDbn |> List.map (fun x -> (x.Alpha, x.Momentum)) 
+        |> List.forall (fun x -> x = (1.0, 0.0)) 
+        |> should equal true
--- DeepBelief/DeepBeliefNet.fs
+++ DeepBelief/DeepBeliefNet.fs
@@ -2,12 +2,53 @@
 
 module DeepBeliefNet =
 
-    let dbnTrain trainLayer stepUp (dbn : List<'a>) xInputs =
-        let start = trainLayer (dbn.Head, xInputs)
-        dbn.Tail |> List.fold(fun (acc : List<'b * 'c>) element -> 
+    open Alea.CUDA
+    open MathNet.Numerics.Random
+    open MathNet.Numerics.Distributions
+    open MathNet.Numerics.LinearAlgebra.Double
+    open MathNet.Numerics.LinearAlgebra.Generic
+
+    type RestrictedBoltzmannMachine = {
+        Alpha : float
+        Momentum : float
+        Weights : Matrix<float>
+        DWeights : Matrix<float>
+        VisibleBiases : Vector<float>
+        DVisibleBiases : Vector<float>
+        HiddenBiases : Vector<float>
+        DHiddenBiases : Vector<float>
+    }
+
+    let dbnTrain trainLayer stepUp (rbms : List<'a>) xInputs =
+        let start = trainLayer (rbms.Head, xInputs)
+        rbms.Tail |> List.fold(fun (acc : List<'b * 'c>) element -> 
             let currentTuple = acc.Head
             let x = stepUp (fst currentTuple) (snd currentTuple)
-            let nextDbn = trainLayer (element, x)
-            (nextDbn, x) :: acc) [(start, xInputs)]
+            let nextRbm = trainLayer (element, x)
+            (nextRbm, x) :: acc) [(start, xInputs)]
             |> List.rev
+
+    let initRbm nVisible nHidden alpha momentum =
+        { 
+            Alpha = alpha; 
+            Momentum = momentum; 
+            Weights = DenseMatrix.zeroCreate nHidden nVisible;
+            DWeights = DenseMatrix.zeroCreate nHidden nVisible;
+            VisibleBiases = DenseVector.zeroCreate nVisible;
+            DVisibleBiases = DenseVector.zeroCreate nVisible;
+            HiddenBiases = DenseVector.zeroCreate nVisible;
+            DHiddenBiases = DenseVector.zeroCreate nVisible
+        }
+
+    let dbn sizes alpha momentum (xInputs : Matrix<float>) =
+        sizes |> List.fold(fun acc element -> 
+            let nVisible = fst acc
+            let nHidden = element
+            (element, (initRbm nVisible nHidden alpha momentum) :: snd acc))
+            (xInputs.ColumnCount, [])
+            |> snd
+            |> List.rev
+
+    let rbmTrain (rbm : RestrictedBoltzmannMachine) (xInputs : DevicePtr<float32>) (xCols : int) =
+        rbm
 
