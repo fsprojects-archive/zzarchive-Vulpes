e0557d509dabcbfe36706455cde2e1725f7cf1f4
spiegelsoft <rob.lyndon@gmail.com>
2014-05-05 23:12:44 +0100

    Added types for backpropagation layer.

--- Backpropagation/Library1.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace Backpropagation
-
-type Class1() = 
-    member this.X = "F#"
---
+++ Backpropagation/Parameters.fs
@@ -0,0 +1,9 @@
+﻿namespace Backpropagation
+
+module Parameters =
+
+    type InputSignal = InputSignal of float32[]
+
+    type OutputLabel = OutputLabel of float32[]
+
+    type TrainingData = TrainingData of (InputSignal * OutputLabel)[]
