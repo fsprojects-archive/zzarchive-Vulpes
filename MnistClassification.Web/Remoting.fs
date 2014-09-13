3b8eb8af64a803f9ea2370f9d0bb6650c212c879
spiegelsoft <rob.lyndon@gmail.com>
2014-09-13 09:29:49 +0100

    Working on dataset visualisation.

--- MnistClassification.Web/Remoting.fs
+++ MnistClassification.Web/Remoting.fs
@@ -7,14 +7,28 @@ open IntelliFactory.WebSharper.Sitelets
 open System.Web
 
 module Remoting =
-    open MnistClassification
+    open DeepBelief.ImageClassification
+    open MnistClassification.MnistDataLoad
+    open System.Collections.Concurrent
+    open System
 
-    let pageKey page =
-        "td" + page
+    let pageKey page = "td" + page
+    let readers = dict [ ("mnist-classification", fun () -> loadMnistDataSet TrainingData) ]
+    let (|?) lhs rhs = (if lhs = null then rhs else lhs)
+
+    let readImageSet (imageSets : ConcurrentDictionary<string, Lazy<ImageSet>>) key =
+        let lazyImageSet = imageSets.GetOrAdd(key, (fun k -> new Lazy<ImageSet>(readers.[k])))
+        lazyImageSet.Value
+
+    let getCachedImageSets() =
+        let imageSets = new ConcurrentDictionary<string, Lazy<ImageSet>>()
+        HttpContext.Current.Cache.Insert("imagesets", imageSets)
+        imageSets
 
     [<Remote>]
     let TrainingData page =
         async {
-            let s = HttpContext.Current.Cache.Get <| pageKey page
+            let imageSets = HttpContext.Current.Cache.Get <| "imagesets" :?> ConcurrentDictionary<string, Lazy<ImageSet>> |? getCachedImageSets()
+            let mnist = readImageSet imageSets "mnist-classification"
             return "You said: " + page
         }
